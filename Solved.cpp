03ca5269e47a213e08c3d2abb2dec81b9415aaac
dvnnyboi <92710617+dvnnyboi@users.noreply.github.com>
2023-07-28 09:18:03 +0900

    11286 : 절댓값 힙 (우선순위 큐)

--- Solved.cpp
+++ Solved.cpp
@@ -645,3 +645,39 @@ int main() {
 	return 0;
 }
 #endif
+
+// 11286 : Àý´ñ°ª Èü
+#if 0
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct cmp {
+	bool operator()(int a, int b) {
+		if (abs(a) == abs(b)) return a > b;
+		else return abs(a) > abs(b);
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	priority_queue<int, deque<int>, cmp> pq;
+	while (n--) {
+		int temp;
+		cin >> temp;
+		if (temp == 0) {
+			if (pq.empty()) cout << '0' << '\n';
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else pq.push(temp);
+	}
+
+	return 0;
+}
+#endif
