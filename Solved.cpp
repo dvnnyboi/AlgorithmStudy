fab5f29461e3faedc430e90456b742527ff6e3d0
dvnnyboi <92710617+dvnnyboi@users.noreply.github.com>
2023-10-26 11:09:24 +0900

    11660 : 구간 합 구하기 5

--- Solved.cpp
+++ Solved.cpp
@@ -1373,3 +1373,49 @@ void post(char r) {
 }
 
 #endif
+
+// 11660 : 구간 합 구하기 5
+#if 0
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+	int dx[4] = { 0, 1, 0, -1 };
+	int dy[4] = { 1, 0, -1, 0 };
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> arr(n + 1, vector<int>(n + 1, 0));
+	vector<vector<int>> sumArr(n + 1, vector<int>(n + 1, 0));
+
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	sumArr[1][1] = arr[1][1];
+	for (int i = 2; i <= n; i++) {
+		sumArr[1][i] = sumArr[1][i - 1] + arr[1][i];
+		sumArr[i][1] = sumArr[i - 1][1] + arr[i][1];
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 2; j <= n; j++) {
+			sumArr[i][j] = sumArr[i][j - 1] + sumArr[i - 1][j] + arr[i][j] - sumArr[i - 1][j - 1];
+		}
+	}
+
+	for (int i = 0; i < m; i++) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+
+		cout << sumArr[x2][y2] - sumArr[x2][y1 - 1] - sumArr[x1 - 1][y2] + sumArr[x1 - 1][y1 - 1] << '\n';
+	}
+
+
+	return 0;
+}
+
+#endif
