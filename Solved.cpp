f92c5b9b179d6bea35f7d52ce1f1de08a12012ed
Daniel Lim <92710617+dvnnyboi@users.noreply.github.com>
2023-12-19 12:20:37 +0900

    9465 : 스티커

--- Solved.cpp
+++ Solved.cpp
@@ -1419,3 +1419,51 @@ int main() {
 }
 
 #endif
+
+// 9465 : ½ºÆ¼Ä¿
+#if 0
+#include <bits/stdc++.h>
+using namespace std;
+
+int stickers[2][100001] = { 0, };
+int dp[2][100001] = { 0, };
+
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+	int t, n;
+	cin >> t;
+	while (t--) {
+		cin >> n;
+
+		for (int i = 0; i < 2; i++) {
+			memset(stickers[i], 0, sizeof(int) * 100001);
+			memset(dp[i], 0, sizeof(int) * 100001);
+		}
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 1; j <= n; j++) {
+				cin >> stickers[i][j];
+			}
+		}
+
+
+		int maxVal = -1;
+		for (int i = 0; i < n; i++) {
+			dp[1][i + 1] = max(dp[1][i + 1], dp[0][i] + stickers[1][i + 1]);
+			dp[1][i + 2] = max(dp[1][i + 2], dp[0][i] + stickers[1][i + 2]);
+			maxVal = max(maxVal, max(dp[1][i + 1], dp[1][i + 2]));
+
+			dp[0][i + 1] = max(dp[0][i + 1], dp[1][i] + stickers[0][i + 1]);
+			dp[0][i + 2] = max(dp[0][i + 2], dp[1][i] + stickers[0][i + 2]);
+			maxVal = max(maxVal, max(dp[0][i + 1], dp[0][i + 2]));
+
+		}
+		cout << maxVal << '\n';
+	}
+
+	return 0;
+}
+
+#endif
--- Solving.cpp
+++ Solving.cpp
@@ -4,41 +4,6 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-	int t;
-	cin >> t;
-
-	while (t--) {
-		int n;
-		cin >> n;
-		vector<vector<int>> stickers(2, vector<int>(n, 0));
-		vector<vector<int>> loss(2, vector<int>(n, 0));
-		vector<vector<bool>> picked(2, vector<bool>(n, false));
-
-
-		int dx[4] = {0, 1, 0, -1};
-		int dy[4] = {1, 0, -1, 0};
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < n; j++) {
-				cin >> stickers[i][j];
-			}
-		}
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int k = 0; k < 4; k++) {
-					int nx = i + dx[k];
-					int ny = j + dy[k];
-
-					if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-
-					loss[i][j] += stickers[nx][ny];
-				}
-			}
-		}
-		
-		
-		
-
-	}
-
+	
 	return 0;
 }
