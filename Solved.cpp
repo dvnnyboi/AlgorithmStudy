70d70b9b4ec0fc2861b86a3410480842cf9e2760
Daniel Lim <imda95@naver.com>
2024-01-11 11:07:59 +0900

    9657: 돌 게임 3

--- Solved.cpp
+++ Solved.cpp
@@ -1803,3 +1803,35 @@ int main() {
 }
 
 #endif
+
+// 9657: µπ ∞‘¿” 3
+#if 0
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[1001] = {};
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[3] = 1;
+	dp[4] = 1;
+	for (int i = 5; i <= n; i++) {
+		if (dp[i - 1] == 0 || dp[i - 3] == 0 || dp[i - 4] == 0) {
+			dp[i] = 1;
+		}
+		else dp[i] = 0;
+	}
+
+	if (dp[n] == 0) cout << "CY";
+	else cout << "SK";
+
+	return 0;
+}
+
+#endif
